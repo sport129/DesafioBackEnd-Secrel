491ef5ec20a51f6cfabec77ab8b6d24d97ed4268
Daniel Rosendo <daniel.rosendos@hotmail.com>
2018-12-19 14:56:47 -0300

    Aplicação Final v1.0

--- app/app.js
+++ app/app.js
@@ -1,25 +1,25 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+//var favicon = require('serve-favicon');
 var logger = require('morgan');
-var express-session = require('express-session');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
 var passport = require('passport');
 var methodOverride = require('method-override');
-
-var auth = require('./routes/auth');
-var tarefas = require('./routes/tarefas');
 
 require('./passport');
 
 var expressNunjucks = require('express-nunjucks');
 
+var auth = require('./routes/auth');
+var tasks = require('./routes/tarefas');
+
 var app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'njk');
-var njk = expressNunjucks(app, {watch: true, noCache: true});
+var njk = expressNunjucks(app,{watch:true, noCache: true});
 
 app.use(session({
   secret: 'teste sessoes',
@@ -31,9 +31,10 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
+
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -47,11 +48,13 @@ app.use(methodOverride(function (req, res) {
 }));
 
 app.use('/', auth);
-app.use('/users', tarefas);
+app.use('/', tasks);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
 // error handler
