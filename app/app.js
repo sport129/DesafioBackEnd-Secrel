aaec915451c000d358946390ddfd74fb7e8d522b
Daniel Rosendo <daniel.rosendos@hotmail.com>
2018-12-19 14:08:11 -0300

    Configurações Iniciais de Autenticação

--- app/app.js
+++ app/app.js
@@ -3,15 +3,33 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var express-session = require('express-session');
+var passport = require('passport');
+var methodOverride = require('method-override');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var auth = require('./routes/auth');
+var tarefas = require('./routes/tarefas');
+
+require('./passport');
+
+var expressNunjucks = require('express-nunjucks');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'njk');
+var njk = expressNunjucks(app, {watch: true, noCache: true});
+
+app.use(session({
+  secret: 'teste sessoes',
+  resave: false,
+  saveUninitialized: false,
+
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -19,8 +37,17 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use(methodOverride(function (req, res) {
+  if (req.body && typeof req.body === 'object' && '_method' in req.body) {
+
+    var method = req.body._method
+    delete req.body._method
+    return method
+  }
+}));
+
+app.use('/', auth);
+app.use('/users', tarefas);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
